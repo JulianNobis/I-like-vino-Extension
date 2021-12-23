4b75f5fd2d9bab284f4a45582ebae877ef7b923f
JulianNobis <julian09022001@gmail.com>
2021-12-23 15:58:35 +0100

    get posts (error)

--- background.js
+++ background.js
@@ -1,12 +1,19 @@
 console.log('from background');
-chrome.tabs.onActivated.addListener(tab => {
-    chrome.tabs.get(tab.tabId, current_tab_info => {
-        console.log(current_tab_info.url);
-        if (/^https:\/\/www\.instagram/.test(current_tab_info.url)){
-            chrome.tabs.executeScript(null, {file: './foreground,js'}, () => {
-                console.log('I injected the script');
-            });
-        }
+
+//TODO: don't forget to scroll down (to load the next divs) and then LOOP it!!!
+
+chrome.runtime.onInstalled.addListener((reason) => {
+    chrome.tabs.create({
+        url: 'https://www.instagram.com/weindieler',
+        active: true
+    }, function(tab) {
+        getPosts();
+        console.log("callback onInstalled create="+tab, tab.title, tab.url)
     });
 });
 
+function getPosts(){
+    console.log("I (should) get posts");
+    var divs = document.getElementsByClassName('v1Nh3 kIKUG  _bz0w'); //ERROR: class name changes constantly!!
+    console.log(divs);
+}
