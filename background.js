117b8d9e6f26e469857ff27f65963aed190ab2f0
JulianNobis <julian09022001@gmail.com>
2021-12-27 12:25:48 +0100

    restructure/simplify code

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
 console.log('from background');
 
-//TODO: don't forget to scroll down (to load the next divs) and then LOOP it!!!
+// TODO: don't forget to scroll down (to load the next divs) and then LOOP it!!!
+// alternatively try to retrieve the posts via instagram api and like them if like-button interaction works
 
 chrome.runtime.onInstalled.addListener((reason) => {
     chrome.tabs.create({
-        url: 'https://www.instagram.com/weindieler',
+        //url: 'https://www.instagram.com/weindieler',
+        url: 'https://www.instagram.com/p/CX-ZbozNMQ5/', // url of a specific post to see if like-button can be interacted with
         active: true
     }, function(tab) {
         getPosts();
@@ -14,6 +16,6 @@ chrome.runtime.onInstalled.addListener((reason) => {
 
 function getPosts(){
     console.log("I (should) get posts");
-    var divs = document.getElementsByClassName('v1Nh3 kIKUG  _bz0w'); //ERROR: class name changes constantly!!
+    var divs = document.querySelectorAll('wpO6b  '); //ERROR: class name changes constantly!!
     console.log(divs);
 }
